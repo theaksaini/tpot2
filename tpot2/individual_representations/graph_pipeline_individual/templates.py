729a339f6a25dee4c28896fbbdb1754015e030db
Anil Saini <theaksaini@gmail.com>
2023-04-28 12:03:10 -0700

    crossover and mutation methods

--- tpot2/individual_representations/graph_pipeline_individual/templates.py
+++ tpot2/individual_representations/graph_pipeline_individual/templates.py
@@ -53,6 +53,11 @@ def estimator_graph_individual_generator(
                             func = np.random.choice(starting_ops)
                             func()
 
+<<<<<<< HEAD:tpot2/individual_representations/graph_pipeline_individual/templates.py
+=======
+                for _ in range(np.random.randint(0,ind.graph.number_of_nodes())):
+                     ind._mutate_add_edge()
+>>>>>>> 99b23e4 (crossover and mutation methods):tpot2/representations/graph_pipeline_individual/templates.py
                 
                 yield ind
 
